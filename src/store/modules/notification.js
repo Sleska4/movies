1ff1ef5d2a8203f569c67152eef3727c8cd07e75
sleska4 <shumakov4l@yandex.ru>
2021-04-20 04:54:11 +0300

    Исправил баг

--- src/store/modules/notification.js
+++ src/store/modules/notification.js
@@ -1,12 +1,13 @@
 import mutations from '@/store/mutations';
 
 const { SHOW_TOAST_NOTIFY } = mutations;
+
 const notificationStore = {
   state: {
     messagePool: [],
   },
   getters: {
-    messagePool: ({ messagePool }) => messagePool.length - 1,
+    messagePool: ({ messagePool }) => messagePool[messagePool.length - 1],
   },
   mutations: {
     [SHOW_TOAST_NOTIFY](state, message) {
